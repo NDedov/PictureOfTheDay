c2950a463090a539b62b907e3d8f01ebad9da5eb
NDedov <ndedov@gmail.com>
2022-08-18 23:33:07 +0700

    lesson1. Добавьте описание (приходит с сервера) под фотографией в виде BottomSheet

--- app/src/main/java/com/example/pictureoftheday/view/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/pictureoftheday/view/PictureOfTheDayFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -57,12 +58,12 @@ class PictureOfTheDayFragment : Fragment() {
         }
 
         viewModel.getLiveData().observe(viewLifecycleOwner) {
-            renderData(it)
+            renderData(view, it)
         }
         viewModel.getData(GregorianCalendar().apply { add(Calendar.DATE,-1) }.time)
     }
 
-    private fun renderData(appState: PictureOfTheDayAppState) {
+    private fun renderData(view: View, appState: PictureOfTheDayAppState) {
         when (appState) {
             is PictureOfTheDayAppState.Success -> {
                 val serverResponseData = appState.serverResponseData
@@ -76,6 +77,8 @@ class PictureOfTheDayFragment : Fragment() {
                         placeholder(R.drawable.ic_no_photo_vector)
                         crossfade(true)
                     }
+                    view.findViewById<TextView>(R.id.bottomSheetDescription).text = serverResponseData.explanation
+                    view.findViewById<TextView>(R.id.bottomSheetDescriptionHeader).text = serverResponseData.title
                 }
             }
             is PictureOfTheDayAppState.Loading -> {
